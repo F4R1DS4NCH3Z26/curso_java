fc7acf1499466ee2079b27a7901edec753437671
FASO2000 <faridsanchezoroz@gmail.com>
2025-04-24 12:27:58 -0500

    mejor, estas entendinedo la sistaxsis sigue asi

---
+++ SentenciaIf/src/AplicacionSaludFitness.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class AplicacionSaludFitness {
+    public static void main(String[] args) {
+        var consola = new Scanner(System.in);
+        final var META_PASOS_DIARIO = 10000;
+        final var CALORIAS_POR_PASO = 0.04;
+        System.out.println("*** Aplicacion de Salud y Fitness ***");
+
+        System.out.print("Nombre de usuario: ");
+        var nombre_usuario = consola.nextLine();
+
+        System.out.print("Pasos caminados: ");
+        var pasos_caminados = Integer.parseInt(consola.nextLine());
+
+        var calorias_quemadas = pasos_caminados * CALORIAS_POR_PASO;
+
+        var meta_alcanzada = (pasos_caminados >= META_PASOS_DIARIO) ? "Si :)" : "No :(";
+
+        System.out.printf("""
+                ------------------------------- 
+                - Nombre de usuario: %s
+                - Pasosa caminados: %s / %s
+                - Cumple con la meta? %s
+                --------------------------------
+                """, nombre_usuario, pasos_caminados, META_PASOS_DIARIO, meta_alcanzada);
+
+    }
+}
---
+++ SentenciaIf/src/SistemaReservaHotel.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class SistemaReservaHotel {
+    public static void main(String[] args) {
+        var consola = new Scanner(System.in);
+        System.out.println(" *** Sistemas de reserva *** ");
+
+        System.out.print("Nombre de Cliente:");
+        var nombre_cliente = consola.nextLine();
+
+        System.out.print("¿Cuantos dias de estadia?: ");
+        var dias_estadia = Integer.parseInt(consola.nextLine());
+
+        System.out.print("¿Cuarto con vista al mas? (true/false): ");
+        var vista_mar = Boolean.parseBoolean(consola.nextLine());
+
+        var costo_cuarto = (vista_mar) ? 190.50 : 150.50;
+        var costo_total_cuarto = dias_estadia * costo_cuarto;
+
+        System.out.printf("""
+                ---------RESERVACION---------------
+                - Nombre del Cliente: %s          
+                - Dias de estadia: %s             
+                - ¿Cuarto con vista al mar? : %s  
+                - Precio del cuarto: %.2f         
+                -----------------------------------
+                SALDO TOTAL : %.2f                
+                -----------------------------------
+                """, nombre_cliente, dias_estadia, vista_mar, costo_cuarto, costo_total_cuarto);
+
+
+
+
+    }
+}
